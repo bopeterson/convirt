e5a6806c614dc8db100d680b46940c30ea12c2a4
Bo Peterson <bo.peterson@gmail.com>
2021-01-31 22:18:01 +0100

    added index.html

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Hey Ya 2021-01-31 21:48 macbop')
-})
+
+//app.get('/', (req, res) => {
+//  res.send('Hey Ya 2021-01-31 21:48 macbop')
+//})
+
+app.use(express.static('public'))
+
 app.listen(3000, () => console.log('Server running on port 3000'))
