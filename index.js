311563528a535cd4bef7490e1080d52199ae6d4b
Bo Peterson <bo.peterson@gmail.com>
2021-01-31 21:51:16 +0100

    Setup PM2 step 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('Hey Ya 2')
+  res.send('Hey Ya 2021-01-31 21:48 macbop')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
